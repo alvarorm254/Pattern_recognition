28244624d84dffd1e957089c7d2a4e66a77700a2
Israel Chaparro <ichaparroc@gmail.com>
2018-12-03 05:59:24 -0500

    TODO: tracking and lines

--- Filtro_video.cpp
+++ Filtro_video.cpp
@@ -14,69 +14,85 @@ using namespace std;
 using namespace cv;
 
 
-int main(int argc, char const *argv[]) {
+int main(int argc, char const *argv[])
+{
+  //Read video (or camera) & test
   VideoCapture cap("PadronAnillos_01.avi");
-  if(!cap.isOpened()){
+  if(!cap.isOpened())
+  {
     cout << "Error opening video stream or file" << endl;
     return -1;
   }
-  while(1){
-    Mat frame;
-    Mat edges;
-    Mat gray;
-    // Capture frame-by-frame
-    cap >> frame;
-    // If the frame is empty, break immediately
-    if (frame.empty())
+  
+  //Declare variables
+  Mat frame;
+  Mat edges;
+  Mat gray;
+  Mat ellipse_shape;
+  vector<vector<Point>>contours;
+  vector<Point>center(contours.size());
+  Point2f rect_points[4];
+  
+  while(1)
+  {
+     
+    //read & verify
+    cap>>frame;
+    if(frame.empty())
       break;
-    //IplImage* gray = cvCreateImage(cvGetSize(frame),IPL_DEPTH_8U,1);
-    cvtColor(frame, gray, cv::COLOR_RGB2GRAY);
-    //int param1=strtol(argv[1],NULL,10);
-    //int param2=strtol(argv[2],NULL,10);
-    //int param3=strtol(argv[3],NULL,10);
-    int scale = 1; //TODO: ??
-    int delta = 0; //TODO: ??
-    //Canny(frame, edges, param1, param2, param3, false);
-    //Mat grad_x, abs_grad_x;
-    //int ddepth = CV_16S;
-    //Sobel( frame, grad_x, ddepth,param1, param2, param3, scale, delta, BORDER_DEFAULT );
-    //Sobel( frame, grad_x, ddepth, 1, 0, 3, scale, delta, BORDER_DEFAULT );
-    //convertScaleAbs( grad_x, abs_grad_x );
-    //GaussianBlur( frame, frame, Size( 15, 15 ), 0, 0 );
-    Canny(frame, edges, 200, 200, 3, false);
-    // Display the resulting frame
-    vector<KeyPoint> keypoints;
-    SimpleBlobDetector::Params params;
+    
+    //to gray
+    cvtColor(frame,gray,cv::COLOR_RGB2GRAY);
 
-	// Change thresholds
-	params.minThreshold = 10;
-	params.maxThreshold = 200;
-	 
-	// Filter by Area.
-	params.filterByArea = true;
-	params.minArea = 1500;
-	 
-	// Filter by Circularity
-	params.filterByCircularity = true;
-	params.minCircularity = 0.1;
-	 
-	// Filter by Convexity
-	params.filterByConvexity = true;
-	params.minConvexity = 0.87;
-	 
-	// Filter by Inertia
-	params.filterByInertia = true;
-	params.minInertiaRatio = 0.01;
+    //to edges
+    //Canny(gray,edges,200,200,3);
+    threshold(gray,edges,100,250,THRESH_BINARY);
+    
+    //get the shape of ellipse and search
+    ellipse_shape=getStructuringElement(MORPH_ELLIPSE,Size(4,4),Point(1,1));
+    dilate(edges,edges,ellipse_shape);
+    
+    //find the contours of ellipses, store all points in two level hierachy
+    contours.clear();
+    findContours(edges,contours,CV_RETR_CCOMP,CHAIN_APPROX_NONE,Point(0,0));
+    
+    //clear and resize center
+    center.resize(contours.size(),Point(0,0));
 
-    Ptr<SimpleBlobDetector> detector=SimpleBlobDetector::create(params);
-    detector->detect(edges,keypoints);
-    Mat drawI=edges.clone();
-    for(size_t i=0;i<keypoints.size();i++)
+    //find the rotated rectangles and ellipses for each contour VALID
+    vector<RotatedRect>minRect(contours.size()); //TODO: not re-declare (not urgent)
+    vector<RotatedRect>minEllipse(contours.size()); //TODO: not re-declare (not urgent)
+    int count=0;
+    for(int i=0;i<contours.size();++i) //TODO: to iterators (not urgent)
     {
-        circle(frame,keypoints[i].pt,4,Scalar(180,0,0),-1);
+        if(contours[i].size()>16 && contours[i].size()<256) //ex-ante validation
+        {
+            minEllipse[count]=fitEllipse(Mat(contours[i]));
+            minRect[count]=minAreaRect(Mat(contours[i]));
+            ++count;
+        }
+    }
+	
+	//find the center
+    for(int i=0;i<count;++i)
+    {
+        minRect[i].points(rect_points);
+        center[i]=Point((rect_points[0].x+rect_points[2].x)/2,(rect_points[0].y+rect_points[2].y)/2); 
     }
 
-    //imshow( "Frame", edges );
+    //ex-post validation, distance of center heuristic
+    for(int i=0;i<count;++i)
+        for(int j=0;j<count;++j)
+            if(j!=i)
+                if(sqrt(pow((center[i].x-center[j].x),2)+pow((center[i].y-center[j].y),2))<4)
+                {
+                    ellipse(frame,minEllipse[i],Scalar(0,255,0),1,LINE_AA);
+                    break;
+                }
+                //TODO: if not match delete, for tracking
+    
+    //TODO TODO: tracking of points based on the center and tracing lines
+
     imshow( "Frame", frame );
 
     // Press  ESC on keyboard to exit
@@ -84,6 +100,7 @@ int main(int argc, char const *argv[]) {
     if(c==27)
       break;
     }
+  
   // When everything done, release the video capture object
   cap.release();
 
