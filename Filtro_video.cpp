401f02f6969c85ad744547fc57eb9d9227566ce7
alvarorm254 <40580097+alvarorm254@users.noreply.github.com>
2018-12-04 14:37:55 -0500

    Add files via upload

--- Filtro_video.cpp
+++ Filtro_video.cpp
@@ -31,11 +31,12 @@ int main(int argc, char const *argv[])
   Mat ellipse_shape;
   vector<vector<Point>>contours;
   vector<Point>center(contours.size());
+  vector<Point>center_v;
   Point2f rect_points[4];
   
   while(1)
   {
-     
+    vector<Point>center_v;
     //read & verify
     cap>>frame;
     if(frame.empty())
@@ -45,13 +46,14 @@ int main(int argc, char const *argv[])
     cvtColor(frame,gray,cv::COLOR_RGB2GRAY);
 
     //to edges
-    //Canny(gray,edges,200,200,3);
-    threshold(gray,edges,100,250,THRESH_BINARY);
+    threshold(gray,edges,100,255,THRESH_BINARY);
     
     //get the shape of ellipse and search
-    ellipse_shape=getStructuringElement(MORPH_ELLIPSE,Size(4,4),Point(1,1));
+    ellipse_shape=getStructuringElement(MORPH_ELLIPSE,Size(2,2),Point(1,1));
+    // Aplying closing algorithm 
     dilate(edges,edges,ellipse_shape);
-    
+    erode(edges,edges,ellipse_shape);
+   
     //find the contours of ellipses, store all points in two level hierachy
     contours.clear();
     findContours(edges,contours,CV_RETR_CCOMP,CHAIN_APPROX_NONE,Point(0,0));
@@ -60,6 +62,7 @@ int main(int argc, char const *argv[])
     center.resize(contours.size(),Point(0,0));
 
     //find the rotated rectangles and ellipses for each contour VALID
+
     vector<RotatedRect>minRect(contours.size()); //TODO: not re-declare (not urgent)
     vector<RotatedRect>minEllipse(contours.size()); //TODO: not re-declare (not urgent)
     int count=0;
@@ -86,15 +89,16 @@ int main(int argc, char const *argv[])
             if(j!=i)
                 if(sqrt(pow((center[i].x-center[j].x),2)+pow((center[i].y-center[j].y),2))<4)
                 {
-                    ellipse(frame,minEllipse[i],Scalar(0,255,0),1,LINE_AA);
+		    ellipse(frame,minEllipse[i],Scalar(0,255,0),1,CV_AA);
+		    circle(frame,center[i],3.0,Scalar( 0,255,0),-1, 8 );
+		    center_v.push_back(center[i]);
                     break;
                 }
-                //TODO: if not match delete, for tracking
-    
+    //TODO: if not match delete, for tracking
+    Rect rect = boundingRect(center_v);
+    rectangle( frame, rect.tl(), rect.br(), Scalar( 0,255,0), 2, 8, 0 );
     //TODO TODO: tracking of points based on the center and tracing lines
-
     imshow( "Frame", frame );
-
     // Press  ESC on keyboard to exit
     char c=(char)waitKey(25);
     if(c==27)
