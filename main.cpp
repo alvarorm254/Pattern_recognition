ec6f0d5be4b3e6e934872cbbf1939c220023b5d2
Israel Chaparro <ichaparroc@gmail.com>
2018-12-12 11:42:16 -0500

    with tracking

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 #include <string>
 #include <stdlib.h>
 
+#include<omp.h>
+
 #include <opencv2/opencv.hpp>
 
 #include <opencv2/highgui/highgui.hpp>
@@ -11,7 +13,8 @@
 
 #include <cmath>
 
-#define NUM_ELLIPSES 30
+#define NUM_ELLIPSES 12
+#define THREAD_COUNT 8
 
 using namespace std;
 using namespace cv;
@@ -24,28 +27,31 @@ int main(int argc, char const *argv[])
 		return -1;
 
 	//Declare variables
-	Mat frame,edges,gray,gaussian,ellipse_shape;
+	Mat frame,edges,gray,gaussian,ellipse_shape=getStructuringElement(MORPH_ELLIPSE,Size(2,2),Point(1,1));
 	vector<vector<Point>>contours;
-	vector<Point>center,center_v;
+	vector<Point>center,center_v,center_v2,center_aux;
 	vector<RotatedRect>minRect,minEllipse;
 	Point2f rect_points[4];
 	vector<Vec4i> hierarchy;
-	int i,j,count,aux;
+	int i,j,k,count,aux;
+	Point mean_center;
+
+	double start_time;
 
 	while(1)
 	{
-		/*if(count>24)
-			cin>>count;*/
-		
 		//clear variables
 		contours.clear();
-		center_v.clear();
+		center_v2.clear();
+		center_aux.clear();
 		hierarchy.clear();
 
 		//read and verify
 		cap>>frame;
 		if(frame.empty())
 			break;
+
+		start_time=omp_get_wtime();
 
 		//to gray
 		cvtColor(frame,gray,cv::COLOR_RGB2GRAY);
@@ -56,21 +62,17 @@ int main(int argc, char const *argv[])
 		//to edges
 		//threshold(gaussian,edges,100,255,THRESH_BINARY);
 		edges=integral_threshold(gaussian,0.85);
-		//TODO: integral threshold
-
-		imshow( "edges", edges );
 
 		//dilate and erode for ellipses
-		ellipse_shape=getStructuringElement(MORPH_ELLIPSE,Size(2,2),Point(1,1));
 		erode(edges,edges,ellipse_shape);
 		dilate(edges,edges,ellipse_shape);
-		//dilate(edges,edges,ellipse_shape);
+
 		//find the contours of ellipses
 		findContours(edges,contours,hierarchy,CV_RETR_TREE,CHAIN_APPROX_NONE,Point(0,0)); //can change methods
 		//hierarchy [Next, Previous, First_Child, Parent]
 
-		//filter contours by size and hierarchy
-		for(i=0;i<contours.size();++i)
+		//filter contours by size and HIERARCHY AND SIZE
+		for(i=0;i<(int)contours.size();++i)
 			if(contours[i].size()>256)
 			{
 				//eliminate in childs
@@ -87,7 +89,7 @@ int main(int argc, char const *argv[])
 				hierarchy[i][3]=-1;
 			}
 			else
-				if(contours[i].size()<24)
+				if(contours[i].size()<16)
 				{
 					hierarchy[i][2]=-1;
 					//eliminate in father
@@ -100,8 +102,9 @@ int main(int argc, char const *argv[])
 				}
 
 		//delete false positive contours
-		for(i=0;i<contours.size();++i)
+		for(i=0;i<(int)contours.size();++i)
 			if(contours[i].size()<24 || (hierarchy[i][2]==-1 && hierarchy[i][3]==-1) || contours[i].size()>256)
+			//TODO: verificar || (hierarchy[i][2]==-1 && (hierarchy[i][0]!=-1 || hierarchy[i][1]!=-1)
 			{
 				contours.erase(contours.begin()+i);
 				hierarchy.erase(hierarchy.begin()+i);
@@ -109,58 +112,102 @@ int main(int argc, char const *argv[])
 			}
 		count=contours.size();
 
-		char str[3];
-		sprintf(str,"%d",count);
-		putText(frame,str,Point2f(15,25),FONT_HERSHEY_PLAIN,2,Scalar(0,0,255,255));
-
-		//find the rotated rectangles and ellipses for each contour VALID
+		//find the rotated rectangles, ellipses and centers
 		minRect.resize(count);
 		minEllipse.resize(count);
+		center.resize(count);
 		for(i=0;i<count;++i)
 		{
 			minEllipse[i]=fitEllipse(Mat(contours[i]));
 			minRect[i]=minAreaRect(Mat(contours[i]));
-		}
-
-		//find the center
-		center.resize(count,Point(0,0));
-		for(i=0;i<count;++i)
-		{
 			minRect[i].points(rect_points);
 			center[i]=Point((rect_points[0].x+rect_points[2].x)/2,(rect_points[0].y+rect_points[2].y)/2);
 		}
 
+		//TODO: delete when count>NUM_ELLIPSES, hierarchy can help
+
+		//TODO: for no rastered points, use the moviment to predict the future location
+
 		//ex-post validation, distance of center heuristic
 		for(i=0;i<count;++i)
-		{
 			for(j=0;j<count;++j)
 				if(j!=i)
 					if(sqrt(pow((center[i].x-center[j].x),2)+pow((center[i].y-center[j].y),2))<4)
 					{
 						ellipse(frame,minEllipse[i],Scalar(0,255,0),1,CV_AA);
-						circle(frame,center[i],3.0,Scalar( 0,255,0),-1, 8 );
-						center_v.push_back(center[i]);
+						circle(frame,center[i],3.0,Scalar(0,255,0),-1,8);
+
+						ellipse(frame,minEllipse[j],Scalar(0,255,0),1,CV_AA);
+						circle(frame,center[j],3.0,Scalar(0,255,0),-1,8);
+
+						mean_center=Point((center[i].x+center[j].x)/2,(center[i].y+center[j].y)/2);
+
+						aux=0;
+						for(k=0;k<(int)center_v.size();++k)
+							if(sqrt(pow((center_v[k].x-mean_center.x),2)+pow((center_v[k].y-mean_center.y),2))<8)
+							{
+								center_v[k]=mean_center;
+								aux=1;
+								break;
+							}
+						if(aux==0)
+							center_v2.push_back(mean_center);
+
+						center_aux.push_back(mean_center);
+
+						if(i<j)
+						{
+							center.erase(center.begin()+j);
+							minEllipse.erase(minEllipse.begin()+j);
+							minRect.erase(minRect.begin()+j);
+
+							center.erase(center.begin()+i);
+							minEllipse.erase(minEllipse.begin()+i);
+							minRect.erase(minRect.begin()+i);
+						}
+						else
+						{
+							center.erase(center.begin()+i);
+							minEllipse.erase(minEllipse.begin()+i);
+							minRect.erase(minRect.begin()+i);
+
+							center.erase(center.begin()+j);
+							minEllipse.erase(minEllipse.begin()+j);
+							minRect.erase(minRect.begin()+j);
+							i--;
+						}
+
+						i--;
+						count=count-2;
+
 						break;
 					}
-		}
 
-		for(i=0;i<count;++i)
+		if(center_v.size()==0)
+			center_v=center_v2;
+
+		if(center_v2.size()>3)
+			center_v=center_aux;
+		else
+			if(center_v.size()<12)
+				center_v.insert(center_v.end(),center_v2.begin(),center_v2.end());
+		//put in rame some important data
+		for(i=0;i<(int)center_v.size();++i)
 		{
 			char str2[3];
 			sprintf(str2,"%d",i);
-			putText(frame,str2,contours[i][0],FONT_HERSHEY_PLAIN,0.5,Scalar(0,0,255,255));
+			putText(frame,str2,center_v[i],FONT_HERSHEY_PLAIN,2,Scalar(0,0,255,255),2);
 		}
 
-
-		//TODO: if not match delete, for tracking
-		Rect rect = boundingRect(center_v);
-		rectangle( frame, rect.tl(), rect.br(), Scalar( 0,255,0), 2, 8, 0 );
+		char str[10];
+		sprintf(str,"%f",omp_get_wtime()-start_time);
+		putText(frame,str,Point2f(15,25),FONT_HERSHEY_PLAIN,1.5,Scalar(0,0,255,255),2);
 
 		//TODO TODO: tracking of points based on the center and tracing lines
 		imshow( "Frame", frame );
 
 		// Press  ESC on keyboard to exit
-		char c=(char)waitKey(25);
+		char c=(char)waitKey(1);
 		if(c==27)
 			break;
 	}
@@ -170,5 +217,6 @@ int main(int argc, char const *argv[])
 
 	// Closes all the frames
 	destroyAllWindows();
+
 	return 0;
 }
