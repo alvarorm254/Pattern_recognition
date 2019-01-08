3d41f67762113dcfebafc8ed5ac14e1764b816cf
Israel Chaparro <ichaparroc@gmail.com>
2019-01-08 05:17:24 -0500

    Add
    ddd

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-//g++ main.cpp -o app `pkg-config --cflags --libs opencv`
+//g++ main.cpp -o app `pkg-config --cflags --libs opencv` -fopenmp
 #include <iostream>
 #include <string>
 #include <stdlib.h>
@@ -13,7 +13,10 @@
 
 #include <cmath>
 
-#define NUM_ELLIPSES 12
+#define ROWS 4
+#define COLS 5
+#define NUM_RINGS ROWS*COLS
+#define NUM_ELLIPSES ROWS*COLS*2
 #define THREAD_COUNT 8
 
 using namespace std;
@@ -22,7 +25,7 @@ using namespace cv;
 int main(int argc, char const *argv[])
 {
 	//Read video (or camera) & test
-	VideoCapture cap("../padron1.avi");
+	VideoCapture cap("../padron2.avi");
 	if(!cap.isOpened())
 		return -1;
 
@@ -35,12 +38,13 @@ int main(int argc, char const *argv[])
 	vector<Vec4i> hierarchy;
 	int i,j,k,count,aux,max_radio;
 	Point mean_center;
+	vector<int> finded(NUM_RINGS,0);
 
-	char time[15],id[3];
+	char time[15],id[11];
 
 	double start_time;
 
-	int all=0,full=0;
+	int all=0,full=0,retrack=0;
 
 	char c=(char)waitKey(3000);
 
@@ -70,15 +74,15 @@ int main(int argc, char const *argv[])
 		//threshold(gaussian,edges,100,255,THRESH_BINARY);
 		edges=integral_threshold(gaussian,0.85);
 
-		//dilate and erode for ellipses
+		/*dilate and erode for ellipses
 		erode(edges,edges,ellipse_shape);
-		dilate(edges,edges,ellipse_shape);
+		dilate(edges,edges,ellipse_shape);*/
 
 		//find the contours of ellipses
 		findContours(edges,contours,hierarchy,CV_RETR_TREE,CHAIN_APPROX_NONE,Point(0,0)); //can change methods
 		//hierarchy [Next, Previous, First_Child, Parent]
 
-		//filter contours by size and HIERARCHY AND SIZE
+		//filter contours by size and HIERARCHY AND SIZE **24 < size < 256**
 		for(i=0;i<(int)contours.size();++i)
 			if(contours[i].size()>256)
 			{
@@ -96,7 +100,7 @@ int main(int argc, char const *argv[])
 				hierarchy[i][3]=-1;
 			}
 			else
-				if(contours[i].size()<16)
+				if(contours[i].size()<24)
 				{
 					hierarchy[i][2]=-1;
 					//eliminate in father
@@ -108,7 +112,7 @@ int main(int argc, char const *argv[])
 					}
 				}
 
-		//delete false positive contours
+		//delete false positive contours in sizes **24 < size < 256** or if no have childrens or father
 		for(i=0;i<(int)contours.size();++i)
 			if(contours[i].size()<24 || (hierarchy[i][2]==-1 && hierarchy[i][3]==-1) || contours[i].size()>256)
 			//TODO: verificar || (hierarchy[i][2]==-1 && (hierarchy[i][0]!=-1 || hierarchy[i][1]!=-1)
@@ -117,15 +121,16 @@ int main(int argc, char const *argv[])
 				hierarchy.erase(hierarchy.begin()+i);
 				--i;
 			}
+
 		count=contours.size();
 
 		//TODO: delete when count>NUM_ELLIPSES, hierarchy can help
-
 
 		//find the rotated rectangles, ellipses and centers
 		minRect.resize(count);
 		minEllipse.resize(count);
 		center.resize(count);
+		
 		max_radio=0;
 		for(i=0;i<count;++i)
 		{
@@ -140,7 +145,9 @@ int main(int argc, char const *argv[])
 
 		}
 
-		//TODO: for no rastered points, use the moviment to predict the future location
+		//TODO: using bounding boxes for teletransportation of bad friend
+
+		//TODO: for no rastered points, use the moviment of another points to predict the future location
 
 		//ex-post validation, distance of center heuristic
 		for(i=0;i<count;++i)
@@ -166,9 +173,11 @@ int main(int argc, char const *argv[])
 							}
 						if(aux==0)
 							center_loss.push_back(mean_center);
+						else
+							finded[k]=1;
 						center_aux.push_back(mean_center);
 
-						if(i<j)
+						if(i<j) //delete the two matched ellipses
 						{
 							center.erase(center.begin()+j);
 							minEllipse.erase(minEllipse.begin()+j);
@@ -196,14 +205,19 @@ int main(int argc, char const *argv[])
 						break;
 					}
 
-		if(center_v.size()==0)
+		if(center_v.size()==0) //only when is the first frame or no one are matched
+		{
 			center_v=center_loss;
+			fill(
+		}
 
 		if(center_loss.size()>2)
+		{
 			center_v=center_aux;
-
+			retrack++;
+		}
 		else
-			if(center_v.size()<12)
+			if(center_v.size()<NUM_RINGS)
 				center_v.insert(center_v.end(),center_loss.begin(),center_loss.end());
 
 		for(i=0;i<(int)center_v.size();++i)
@@ -214,26 +228,30 @@ int main(int argc, char const *argv[])
 
 		//put in rame some important data
 
-		sprintf(time,"Time: %.3f",omp_get_wtime()-start_time);
-		putText(frame,time,Point2f(15,25),FONT_HERSHEY_PLAIN,1.5,Scalar(0,0,255,255),2);
-
-		if(center_v.size()==12)
+		if(center_v.size()==NUM_RINGS)
 			full++;
 		all++;
 
 		sprintf(time,"acc: %.2f",100.0*(float)full/float(all));
 		putText(frame,time,Point2f(15,55),FONT_HERSHEY_PLAIN,1.5,Scalar(0,0,255,255),2);
 
+		sprintf(time,"Time: %.3f",omp_get_wtime()-start_time);
+		putText(frame,time,Point2f(15,25),FONT_HERSHEY_PLAIN,1.5,Scalar(0,0,255,255),2);
+
 		//TODO TODO: tracking of points based on the center and tracing lines
 		imshow( "Frame", frame );
 
 		// Press  ESC on keyboard to exit
-		char c=(char)waitKey(1);
+		char c=(char)waitKey(10);
 		if(c==27)
 			break;
 	}
 
-	cout<<100.0*(float)full/float(all);
+	cout<<"\nNumber of frames: "<<all;
+
+	cout<<"\nPrecision:"<<100.0*(float)full/float(all);
+
+	cout<<"\nRe-track:"<<retrack;
 
 	// When everything done, release the video capture object
 	cap.release();
